a47610a5b1086ae5472f66e65170f1deb8b2b039
Dnyaneshwar Dadabhau Patil <dnyaneshwardpatil2003@gmail.com>
2025-09-13 14:10:57 +0530

    Delete auth.routes.js

--- auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { signup, login } = require("../controllers/auth.controller");
-
-const router = express.Router();
-
-router.post("/signup", signup); // Normal user signup
-router.post("/login", login);   // Login for all roles
-
-module.exports = router;
