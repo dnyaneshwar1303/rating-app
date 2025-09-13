6eed8f95e61107e0252557ce18bb74274dcc4871
Dnyaneshwar Dadabhau Patil <dnyaneshwardpatil2003@gmail.com>
2025-09-13 14:18:28 +0530

    Delete owner.routes.js

--- owner.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getOwnerDashboard } = require("../controllers/owner.controller");
-const authMiddleware = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-router.get("/dashboard", authMiddleware(["OWNER"]), getOwnerDashboard);
-
-module.exports = router;
