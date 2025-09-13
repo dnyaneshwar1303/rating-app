588532878bb225957079f35ab24f521b82af93ea
Dnyaneshwar Dadabhau Patil <dnyaneshwardpatil2003@gmail.com>
2025-09-13 14:09:21 +0530

    Delete admin.routes.js

--- admin.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getDashboard } = require("../controllers/admin.controller");
-const authMiddleware = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-router.get("/dashboard", authMiddleware(["ADMIN"]), getDashboard);
-
-module.exports = router;
