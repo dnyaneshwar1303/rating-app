557bbca3a8b38b1f014012ede058dfc073a019df
Dnyaneshwar Dadabhau Patil <dnyaneshwardpatil2003@gmail.com>
2025-09-13 14:25:43 +0530

    Delete rating.routes.js

--- rating.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { submitRating } = require("../controllers/rating.controller");
-const authMiddleware = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-router.post("/", authMiddleware(["USER"]), submitRating);
-
-module.exports = router;
